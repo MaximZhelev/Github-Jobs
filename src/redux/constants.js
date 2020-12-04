877b2243adf08d8d90210a01bb0cd5bc7e067591
Maxim Zhelev <maxim.jelev@gmail.com>
2020-12-04 20:37:32 +0200

    version 1.0.0

---
+++ src/redux/constants.js
@@ -0,0 +1,8 @@
+export const BASE_URL = "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json"
+
+export const ACTIONS = {
+    MAKE_REQUEST: "MAKE_REQUEST",
+    GET_DATA: "GET_DATA",
+    ERROR: "ERROR",
+    UPDATE_HAS_NEXT_PAGE: "UPDATE_HAS_NEXT_PAGE"
+  };
